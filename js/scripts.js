291649cf25924f08867fec9d5a98d427be73b5f7
abolzz <abolzzy@gmail.com>
2018-03-15 19:19:04 +0100

    All files added

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// Scripts
+
+// Toggles between showing and hiding mobile menu
+document.getElementById("navBurger").onclick = function() {
+    document.getElementById("menuItems").classList.toggle("show");
+    document.getElementById("menuItems").classList.toggle("hide");
+}
